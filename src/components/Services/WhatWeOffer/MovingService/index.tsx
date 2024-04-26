9830681895e879000c94cd8a5e237769193bca83
innovative mojo <developer@innovativemojo.com>
2024-04-26 18:28:26 +0500

    services responsive

--- src/components/Services/WhatWeOffer/MovingService/index.tsx
+++ src/components/Services/WhatWeOffer/MovingService/index.tsx
@@ -8,24 +8,24 @@ import Text from "@/components/ui/Text";
 const MovingService = () => {
   return (
     <>
-      <div className="w-full bg-[#191A05] min-h-[400px] flex flex-wrap gap-20 justify-center items-center py-4 mob:px-5">
-        <div className="flex flex-col">
+      <div className="w-full bg-[#191A05] min-h-[400px] flex flex-wrap gap-20 justify-center items-center py-4 mob:px-5 mob:py-5">
+        <div className="flex flex-col mob:justify-center">
           <Image src={movingimg} alt="" width={339} height={281} />
         </div>
         <div className="flex flex-col">
-          <div className="w-full max-w-[515px]">
+          <div className="w-full max-w-[515px] mob:max-w-full">
             <Text
               as="h1"
-              className="text-[#E2E1DB] text-[65px] leading-[54.6px] flex items-center uppercase"
+              className="text-[#E2E1DB] text-[65px] leading-[54.6px] flex items-center uppercase mob:justify-center mob:text-center"
             >
               01 <span className="text-[32px]  ml-4">Moving Services</span>
             </Text>
-            <Text as="p" className="text-[#E2E1DB] text-[17px] leading-[27px] ">
+            <Text as="p" className="text-[#E2E1DB] text-[17px] leading-[27px] mob:text-center">
               You dont have to pick between affordable rates or quality
               service. Trust Bulk Brothers to get your items to your new home
               safely and efficiently at affordable rates.
             </Text>
-            <div className="flex mt-4">
+            <div className="flex mob:justify-center mt-4">
               <Image src={star} alt="" width={17} height={17} />
               <Text
                 as="p"
@@ -34,7 +34,7 @@ const MovingService = () => {
                 Residential Business
               </Text>
             </div>
-            <div className="flex mt-1">
+            <div className="flex mob:justify-center mt-1">
               <Image src={star} alt="" width={17} height={17} />
               <Text
                 as="p"
--- src/components/Services/WhatWeOffer/TrashMover/index.tsx
+++ src/components/Services/WhatWeOffer/TrashMover/index.tsx
@@ -8,7 +8,7 @@ import Text from "@/components/ui/Text";
 const MovingService = () => {
   return (
     <>
-      <div className="w-full bg-[#191A05] min-h-[400px] flex flex-wrap gap-20 justify-center items-center py-4 mob:px-5">
+      <div className="w-full bg-[#191A05] min-h-[400px] flex flex-wrap gap-20 justify-center items-center py-4 mob:px-5 mob:py-5">
         <div className="flex flex-col">
           <Image src={trashimg} alt="" width={339} height={281} />
         </div>
@@ -16,15 +16,21 @@ const MovingService = () => {
           <div className="w-full max-w-[515px]">
             <Text
               as="h1"
-              className="text-[#E2E1DB] text-[65px] leading-[54.6px] flex items-center uppercase"
+              className="text-[#E2E1DB] text-[65px] leading-[54.6px] flex items-center mob:items-start mob:justify-center mob:text-center uppercase"
             >
-              03 <span className="text-[32px]  ml-4">Trash Removal Services</span>
+              03  <span className="text-[32px] mob:hidden ml-4">Trash Removal Services</span>
             </Text>
-            <Text as="p" className="text-[#E2E1DB] text-[17px] leading-[27px] ">
+            <Text
+              as="h1"
+              className="text-[#E2E1DB] text-[65px] leading-[54.6px] mob:text-center uppercase hidden mob:block "
+            >
+              <span className="text-[32px]  ">Trash Removal Services</span>
+            </Text>
+            <Text as="p" className="text-[#E2E1DB] text-[17px] leading-[27px] mob:text-center">
             Whether large or small, Bulk Brothers can take care of all of your clutter, trash, and junk removal needs. 
             </Text>
             {/* stars */}
-            <div className="flex mt-4">
+            <div className="flex mob:justify-center mt-4">
               <Image src={star} alt="" width={17} height={17} />
               <Text
                 as="p"
@@ -33,7 +39,7 @@ const MovingService = () => {
                 Residential
               </Text>
             </div>
-            <div className="flex mt-1">
+            <div className="flex mob:justify-center mt-1">
               <Image src={star} alt="" width={17} height={17} />
               <Text
                 as="p"
@@ -43,7 +49,7 @@ const MovingService = () => {
               </Text>
             </div>
 
-            <div className="flex mt-1">
+            <div className="flex mob:justify-center mt-1">
               <Image src={star} alt="" width={17} height={17} />
               <Text
                 as="p"
@@ -53,7 +59,7 @@ const MovingService = () => {
 Large and small jobs
               </Text>
             </div>
-            <div className="flex mt-1">
+            <div className="flex mob:justify-center mt-1">
               <Image src={star} alt="" width={17} height={17} />
               <Text
                 as="p"
@@ -63,7 +69,7 @@ Large and small jobs
 House renovation and clean outs
               </Text>
             </div>
-            <div className="flex mt-1">
+            <div className="flex mob:justify-center mt-1">
               <Image src={star} alt="" width={17} height={17} />
               <Text
                 as="p"
@@ -72,7 +78,7 @@ House renovation and clean outs
             Household items
               </Text>
             </div>
-            <div className="flex mt-1">
+            <div className="flex mob:justify-center mt-1">
               <Image src={star} alt="" width={17} height={17} />
               <Text
                 as="p"
