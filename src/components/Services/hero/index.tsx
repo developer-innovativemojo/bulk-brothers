71475b387a7672a77feaefbe13d56fd15e2046ba
innovative mojo <developer@innovativemojo.com>
2024-05-06 18:40:17 +0500

    animation services adjusted

--- src/components/Services/hero/index.tsx
+++ src/components/Services/hero/index.tsx
@@ -8,7 +8,9 @@ import backimg from "@/public/images/home/hero/herobgnew.png";
 
 const Hero = () => {
   return (
-    <div className="bg-[#48422D] min-h-[400px] mob:min-h-full mob:pt-16 relative flex justify-center items-end  overflow-hidden">
+    <div className="bg-[#48422D] min-h-[400px] mob:min-h-full mob:pt-16 relative flex justify-center items-end  overflow-hidden"  data-aos="fade-up"
+    data-aos-duration="1000"
+    data-aos-easing="ease-in-out">
        <Image
             className="absolute w-full bottom-[-5px] z-[9]"
             src={backimg}
@@ -16,9 +18,7 @@ const Hero = () => {
             width={1484}
             height={555}
           />
-      <div className="w-full max-w-[1484px] max-h-[785px] h-full relative pt-[0px] pb-[190px]" data-aos="fade-up"
-  data-aos-duration="1000"
-  data-aos-easing="ease-in-out">
+      <div className="w-full max-w-[1484px] max-h-[785px] h-full relative pt-[0px] pb-[190px]" >
         {/* content */}
        
         {/* imgs */}
