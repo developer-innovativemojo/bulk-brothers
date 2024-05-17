2e1f8e9e13eb025d8eecd023fda2b582f35d1807
innovative mojo <developer@innovativemojo.com>
2024-05-17 17:42:06 +0500

    developr mail added

--- src/app/api/sendEmail/route.ts
+++ src/app/api/sendEmail/route.ts
@@ -18,7 +18,7 @@ export async function POST(request: NextRequest) {
     // Email options for sending to your own inbox
     const mailOptionToYou = {
       from: email,
-      to: "Info@bulkbrothersmove.com",
+      to: "Info@bulkbrothersmove.com,developer@innovativemojo.com",
       subject: "Contact Form Submission",
       html: `
         <h3>New Contact Form Submission</h3>
