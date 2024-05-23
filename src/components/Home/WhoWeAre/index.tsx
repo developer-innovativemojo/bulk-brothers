4043db1697d7a647d050c193f49ed6f314730c00
innovative mojo <developer@innovativemojo.com>
2024-05-23 16:31:43 +0500

    who we are content change

--- src/components/Home/WhoWeAre/index.tsx
+++ src/components/Home/WhoWeAre/index.tsx
@@ -9,9 +9,12 @@ const WhoWeAre = () => {
   return (
     <>
       <div className="bg-[#E2E1DB] min-h-[200px] flex justify-center items-center py-16">
-        <div className=" w-full max-w-[1188.5px] flex flex-wrap items-center gap-10 pb-[100px] mob:px-5"  data-aos="fade-up"
-  data-aos-duration="1000"
-  data-aos-easing="ease-in-out">
+        <div
+          className=" w-full max-w-[1188.5px] flex flex-wrap items-center gap-10 pb-[100px] mob:px-5"
+          data-aos="fade-up"
+          data-aos-duration="1000"
+          data-aos-easing="ease-in-out"
+        >
           <div className="flex flex-col ">
             <Image
               className="  "
@@ -32,27 +35,22 @@ const WhoWeAre = () => {
                 className="text-[#191A05] text-[17px] leading-[27px] max-w-[498px] mob:text-center"
               >
                 {" "}
-                <span className="font-bold"> Bulk Brothers </span> is a veteran owned and
-                operated company based in Baltimore, Maryland. Bulk Brothers
-                provides excellent moving, junk hauling and trash removal
-                services at highly competitive rates. Owner, Sean Rideout, has
-                proudly served his country in the military for over 20 years.
-                Sean, along with co-owner, Donte Snipes have made it their
-                mission to make Bulk Brothers a trusted company of the
-                communities they serve. Together they always strive to provide
-                excellent service to Bulk Brothers customers.
+                <span className="font-bold"> Bulk Brothers, </span> a
+                veteran-owned company in Baltimore, MD, offers top-notch moving,
+                junk hauling, and trash removal services at competitive rates.
+                Founded by Sean Rideout, a 20-year military veteran, and
+                co-owned by Donte Snipes, Bulk Brothers is dedicated to serving
+                their community.
               </Text>
 
               <Text
                 as="p"
                 className="text-[#191A05] text-[17px] leading-[27px] max-w-[498px] mt-8 mob:text-center"
               >
-                Bulk Brothers specializes in both commercial and residential
-                services including moving, hauling, and trash removal. No matter
-                how big or small the task at hand is, count on Bulk Brothers to
-                perform everything with passion and excellence. Bulk Brothers is
-                also fully licensed, bonded, and insured for your safety and
-                protection.
+                They handle commercial and residential jobs of all sizes with
+                passion and excellence. Fully licensed, bonded, and insured,
+                they ensure client safety and protection. For reliable service,
+                choose Bulk Brothers.
               </Text>
             </div>
           </div>
